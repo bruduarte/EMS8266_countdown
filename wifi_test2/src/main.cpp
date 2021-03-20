c84a4e5faa0def60d08b7bf305644f06921ca4d4
bruduarte <brunaduarte.eng@gmail.com>
2021-03-20 21:24:14 +0100

    NTP server working

--- wifi_test2/src/main.cpp
+++ wifi_test2/src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
 #define LED 2
+
+const char* ssid       = "UPC7472663";
+const char* password   = "a6Qmsbhnwrdk";
+
+WiFiUDP ntpUDP;
+int timeOffset = 3600; //+1*60*60
+
+NTPClient timeClient(ntpUDP, timeOffset);
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println();
   pinMode(LED, OUTPUT);
-  WiFi.begin("UPC7472663", "a6Qmsbhnwrdk");
+  WiFi.begin(ssid, password);
 
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -16,17 +26,36 @@ void setup()
     delay(500);
     Serial.print(".");
   }
+
+  timeClient.begin();
   Serial.println();
+
+  Serial.println("trying to get current time from NTP server.");
+
+  bool ret = timeClient.forceUpdate();
+  if(ret){
+    Serial.println("Funfou");
+  }
+  else {
+    Serial.println("Nao funfou");
+  }
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println(timeClient.getFormattedTime());
 }
 
 void loop() {
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());
+
   digitalWrite(LED, HIGH);
   Serial.println("LED is on");
   delay(1000);
   digitalWrite(LED, LOW);
   Serial.println("LED is off");
+
+  Serial.println(timeClient.getFormattedTime());
+
   delay(1000);
 }
