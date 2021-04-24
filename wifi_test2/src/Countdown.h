9087296902845a4affb485471be85ddd07edcc05
bruduarte <brunaduarte.eng@gmail.com>
2021-04-24 22:07:45 +0200

    Correctly showing remaining time in the display!

--- wifi_test2/src/Countdown.h
+++ wifi_test2/src/Countdown.h
@@ -1,4 +1,9 @@
 #include "LocalDatabase.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
 
 class Countdown
 {
@@ -8,7 +13,9 @@ public:
     Countdown(/* args */);
     ~Countdown();
 
-    void displayCountdown(String time, int hour, int min, int sec, timetableEntry *timetable);
+    void displayFormatting(char* char_in, char* char_out, int maxSize); //function to format the characters to be displayed, limits size!
+    void serialDisplayCountdown(String time, int hour, int min, int sec, timetableEntry *timetable);
+    void displayCountdown(Adafruit_SSD1306 &display,String time, int hour, int min, int sec, timetableEntry *timetable); 
 };
 
 
