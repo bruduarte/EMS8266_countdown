54ea7f42e205322b4d9c43b2eca41c08a1e3d8a2
bruduarte <brunaduarte.eng@gmail.com>
2021-05-08 19:21:12 +0200

    new database with real data, new function to load stops, modified load database and other database related functions

--- ESP8266_eclipse/src/LocalDatabase.h
+++ ESP8266_eclipse/src/LocalDatabase.h
@@ -1,7 +1,7 @@
 #ifndef LOCALDATABASE
 #define LOCALDATABASE
 
-/*This class deals with the database of the timtable of the public transportation*/
+/*This class deals with the database of the timetable of the public transportation*/
 
 #include <iostream>
 #include <string.h>
@@ -18,35 +18,50 @@
 #endif
 
 #define MAXSTOPS        5         //maximum number of stops
-#define MAXLINES        3         //maximum number of lines to work with
+#define MAXLINES        7         //maximum number of lines to work with
 #define MAXTIMEPERLINE  (60/5)*10 //maximum number of times the line stops at a station in a day, assuming every 5min.
 // #define DBSIZE  MAXSTOPS*MAXLINES*MAXTIMEPERLINE // maximum size of the database!
-#define DBSIZE  100
+#define DBSIZE  225
 // #define MY_DEBUG
 
-//database entry
+//database entry - schedule
 typedef struct timetableEntry_type
 {
-    char     lineID [4];
-    int      hour;
-    int      min;      
-    char     stopName [20]; //name of the stop
-    int      walkTime; //time it takes from your location to the stop
+    char     routeID [9];	//ID of the route/line
+    int      arriveTime;	//Scheduled time for the route to arrive in the stop
+    char     stopID [11]; 	//ID of the stop
+    int      walkTime; 		//time it takes from your location to the stop
+    char	 day[2]; 			//D = default H = holidays and weekends
 
 }timetableEntry;
+
+typedef struct stopsInfo_type
+{
+	char	stopID [11];
+	char	stopName [27];
+	int		walkTime;
+}stopsInfo;
 
 class LocalDatabase
 {
 private:
-    timetableEntry timetable[DBSIZE];
-    String  line;
+    timetableEntry	timetable[DBSIZE];
+    stopsInfo		stopsInfo[MAXSTOPS];
+    String			line;
 public:
     LocalDatabase();
     ~LocalDatabase();
 
     timetableEntry *getLocalDatabase();
+
+    struct stopsInfo_type *getLocalStopsInfo();
+
+    void loadStopsInfo(const char* stopsFile);
+
+    int walkTimeForStop(char* stopID);
+
     /*This function loads the DB from a csv file and stores the data into the array of struct*/
-    void loadTimetable(const char* filename);
+    void loadTimetable(const char* scheduleFile);
 
     // void saveTimetable();
 
@@ -60,6 +75,8 @@ public:
 
     /*Calls qsort in order sort the array*/
     void sortDatabase();
+
+
 };
 
 
