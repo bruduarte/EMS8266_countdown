79365ea8a498bf1add43672bd225a698d4ab19fa
bruduarte <brunaduarte.eng@gmail.com>
2021-05-08 22:54:41 +0200

    New database, new display

--- wifi_test2/src/Countdown.cpp
+++ wifi_test2/src/Countdown.cpp
@@ -23,6 +23,9 @@ void Countdown::displayFormatting(char* char_in, char* char_out, int maxSize){
     char_out[maxSize]='\0'; //end it with \0
 }
 
+
+
+
 void Countdown::serialDisplayCountdown(String time, int hour, int min, int sec, timetableEntry *timetable){
     
     int currentTime = hour*60*60 + min*60 + sec; // current time in seconds
@@ -32,15 +35,15 @@ void Countdown::serialDisplayCountdown(String time, int hour, int min, int sec, 
     int scheduleTime = 0;
     int displayLimit = 0; //limit to show only 5 entries
 
-    Serial.printf("############### %s ################\n", time.c_str());
+    debug_print("############### %s ################\n", time.c_str());
     for(int i = 0; i < DBSIZE; i++){
         timetableEntry item = timetable[i]; //current timetable line
 
-        scheduleTime = item.hour*60*60 + item.min*60; //schedule time of the transport in seconds
+        scheduleTime = item.arriveTime; //schedule time of the transport in seconds
 
         // Serial.println(scheduleTime);//**************************************TESTE
 
-        int remainingTime = scheduleTime - (currentTime + item.walkTime*60); //time difference in seconds between now + walk time to the station and the schedule of the transport
+        int remainingTime = scheduleTime - (currentTime + item.walkTime); //time difference in seconds between now + walk time to the station and the schedule of the transport
         // Serial.println(remainingTime);//**************************************TESTE
         // Serial.println();
         int remainingTimeHour = remainingTime/3600;
@@ -52,19 +55,67 @@ void Countdown::serialDisplayCountdown(String time, int hour, int min, int sec, 
         /*If you still get time to catch the transport, print to display*/        
         if(remainingTime > 0){
             if(displayLimit < 4){
-                Serial.printf("%s\t\t%s\t%i:%i\t%i:%i:%i \n", item.stopName, item.lineID, item.hour, item.min, remainingTimeHour, remainingTimeMin, remainingTimeSec);
+            	debug_print("%s\t%s\t%i:%i\t%i:%i:%i \n", item.stopID, item.routeID, scheduleTime/3600, (scheduleTime - (scheduleTime/3600)*3600)/60, remainingTimeHour, remainingTimeMin, remainingTimeSec);
                 displayLimit++;
             }
-        }
         
-       
+
+        }
 
     }
-    Serial.println("#########################################");
+    debug_print("#########################################\n\n");
     
 
 
 }
+
+void Countdown::serialDisplayPerStopCountdown(char* stopID,int hour, int min, int sec, timetableEntry *timetable, stopsInfo *stops){
+
+	int currentTime = hour*60*60 + min*60 + sec; // current time in seconds
+	int scheduleTime = 0;
+	int displayLimit = 0; //limit to show only 5 entries
+	char* stopName;
+
+	for (int n = 0; n < MAXSTOPS; n++) {
+		if(strcmp(stopID, stops[n].stopID) == 0){
+			stopName = stops[n].stopName;
+			break;
+		}
+	}
+
+	debug_print("(%s) - %s \n\n",stopID, stopName);
+
+	for(int i = 0; i < DBSIZE; i++){
+		timetableEntry item = timetable[i]; //current timetable line
+
+		if(strcmp(item.stopID, stopID)==0){
+
+			scheduleTime = item.arriveTime; //schedule time of the transport in seconds
+
+			// Serial.println(scheduleTime);//**************************************TESTE
+
+			int remainingTime = scheduleTime - (currentTime + item.walkTime); //time difference in seconds between now + walk time to the station and the schedule of the transport
+
+			int remainingTimeHour = remainingTime/3600;
+			int remainingTimeMin = (remainingTime - remainingTimeHour*3600)/60;
+			int remainingTimeSec = remainingTime - remainingTimeHour*3600 - remainingTimeMin*60;
+
+
+			/*If you still get time to catch the transport, print to display*/
+			if(remainingTime > 0){
+				if(displayLimit < 4){
+					debug_print("%s\t%i:%i\t%i:%i:%i \n", item.routeID, scheduleTime/3600, (scheduleTime - (scheduleTime/3600)*3600)/60, remainingTimeHour, remainingTimeMin, remainingTimeSec);
+					displayLimit++;
+				}
+
+
+			}
+
+		}
+	}
+	debug_print("\n");
+}
+
 
 void Countdown::displayCountdown (Adafruit_SSD1306 &display, String time, int hour, int min, int sec, timetableEntry *timetable){
     display.clearDisplay();
@@ -83,9 +134,9 @@ void Countdown::displayCountdown (Adafruit_SSD1306 &display, String time, int ho
     for(int i = 0; i < DBSIZE; i++){
         timetableEntry item = timetable[i]; //current timetable line
 
-        scheduleTime = item.hour*60*60 + item.min*60; //schedule time of the transport in seconds
+        scheduleTime = item.arriveTime; //schedule time of the transport in seconds
 
-        int remainingTime = scheduleTime - (currentTime + item.walkTime*60); //time difference in seconds between now + walk time to the station and the schedule of the transport
+        int remainingTime = scheduleTime - (currentTime + item.walkTime); //time difference in seconds between now + walk time to the station and the schedule of the transport
 
         int remainingTimeHour = remainingTime/3600;
         int remainingTimeMin = (remainingTime - remainingTimeHour*3600)/60;
@@ -96,13 +147,13 @@ void Countdown::displayCountdown (Adafruit_SSD1306 &display, String time, int ho
         if(remainingTime > 0){
             if(displayLimit < 4){
 
-                char tempStopName[10];
-                char tempLineID[4];
+                // char tempStopName[10];
+                // char tempLineID[4];
 
-                displayFormatting(item.stopName,tempStopName, 9);
-                displayFormatting(item.lineID, tempLineID, 3);
+                // displayFormatting(item.stopName,tempStopName, 9);
+                // displayFormatting(item.lineID, tempLineID, 3);
 
-                display.printf("%s %s %i:%i \n", tempStopName, tempLineID, remainingTimeHour, remainingTimeMin);
+                display.printf("%s %i:%i %i:%i \n", item.routeID, scheduleTime/3600, (scheduleTime - (scheduleTime/3600)*3600)/60, remainingTimeHour, remainingTimeMin);
                 displayLimit++;
                 display.display();
             }
@@ -117,3 +168,70 @@ void Countdown::displayCountdown (Adafruit_SSD1306 &display, String time, int ho
 
 
 }
+
+void Countdown::displayCountdownPerStop(Adafruit_SSD1306 &display, char* stopID,int hour, int min, int sec, timetableEntry *timetable, stopsInfo *stops){
+	display.clearDisplay();
+	display.setCursor(0,0);
+	display.setTextSize(1);
+	display.setTextColor(WHITE);
+    char* stopName = NULL;
+	int currentTime = hour*60*60 + min*60 + sec; // current time in seconds
+	int scheduleTime = 0;
+	int displayLimit = 0; //limit to show only 5 entries
+
+    /*Getting the name of the current stop!*/
+    for (int i = 0; i < MAXSTOPS; i++)
+    {
+        if(strcmp(stopID, stops[i].stopID) == 0){
+            stopName = stops[i].stopName;
+            break;
+        }
+    }
+    
+
+	display.printf("(%s) - %s \n\n", stopID, stopName);
+	display.display();
+
+	for(int i = 0; i < DBSIZE; i++){
+		
+		
+		timetableEntry item = timetable[i]; //current timetable line
+
+        if(strcmp(item.stopID, stopID) == 0){
+
+            scheduleTime = item.arriveTime; //schedule time of the transport in seconds
+
+            int remainingTime = scheduleTime - (currentTime + item.walkTime); //time difference in seconds between now + walk time to the station and the schedule of the transport
+
+            int remainingTimeHour = remainingTime/3600;
+            int remainingTimeMin = (remainingTime - remainingTimeHour*3600)/60;
+         // int remainingTimeSec = remainingTime - remainingTimeHour*3600 - remainingTimeMin*60;
+
+        
+            /*If you still get time to catch the transport, print to display*/        
+            if(remainingTime > 0){
+                if(displayLimit < 4){
+
+                    // char tempStopName[10];
+                    // char tempLineID[4];
+
+                    // displayFormatting(item.stopName,tempStopName, 9);
+                    // displayFormatting(item.lineID, tempLineID, 3);
+
+                    display.printf("%s %i:%i %i:%i \n", item.routeID, scheduleTime/3600, (scheduleTime - (scheduleTime/3600)*3600)/60, remainingTimeHour, remainingTimeMin);
+                    displayLimit++;
+                    display.display();
+                }
+            }
+        }
+
+		
+	   
+
+	}
+	
+	display.display();
+}
+
+
+
